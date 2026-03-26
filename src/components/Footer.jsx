c41c5cc09bb66ad6b48e452056a25555b7a0713f
Rafia <rafia.habib135@gmail.com>
2026-03-27 00:57:27 +0500

    class setting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <div class="card text-center">
-                <div class="card-header">
+            <div className="card text-center">
+                <div className="card-header">
                     Islamic App
                 </div>
-                <div class="card-body">
-                    <h5 class="card-title">Copyright © 2026 Islamic App</h5>
-                    <p class="card-text">Created an Islamic App, to view, and listen to Ayat</p>
+                <div className="card-body">
+                    <h5 className="card-title">Copyright © 2026 Islamic App</h5>
+                    <p className="card-text">Created an Islamic App, to view, and listen to Ayat</p>
                 </div>
             </div>
         </>
